79776355322ce9600e6a89da1b0b626cdfe54aed
kwiasek <kwiasekkwiasek@gmail.com>
2024-08-18 01:58:58 +0200

    Done a lot of changes

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const port = 3000;
 
 let username = undefined;
+let posts = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { username });
 });
 
 app.get("/login", (req, res) => {
-  res.send("Login page");
+  res.render("login.ejs", { username });
 });
 
 app.post("/login", (req, res) => {
-  res.send("Login submitted");
+  username = req.body.username;
+  res.redirect("/");
 });
 
 app.get("/posts", (req, res) => {
-  res.send("Posts page");
+  res.render("posts.ejs", { posts });
 });
 
 app.get("/create", (req, res) => {
-  res.send("Create page");
+  const id = uuidv4();
+  const date = new Date().toDateString();
+  if (!username) {
+    res.redirect("/login");
+  } else {
+    res.render("create.ejs", { username, id, date });
+  }
+});
+
+app.post("/create", (req, res) => {
+  const { id, author, date, title, desc, content } = req.body;
+  const post = { id, author, title, desc, content, date };
+  posts.unshift(post);
+  res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req, res) => {
-  res.send(`Post #${req.params.id}`);
+  const post = posts.find((elem) => elem.id === req.params.id);
+  if (!post) {
+    res.redirect("/posts");
+  }
+  res.render("post.ejs", { post, username });
 });
 
-app.put("/posts/:id/edit", (req, res) => {
-  res.send(`Post #${req.params.id} edit page`);
+app.get("/posts/:id/edit", (req, res) => {
+  const post = posts.find((elem) => elem.id === req.params.id);
+  if (!post || post.author !== username) res.redirect("/posts");
+  res.render("edit.ejs", { post });
 });
 
-app.post("/posts/:id", (req, res) => {
+app.post("/posts/:id/edit", (req, res) => {
   res.send(`Post #${req.params.id} has been edited`);
 });
 
